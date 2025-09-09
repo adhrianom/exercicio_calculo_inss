1464699ef5fafbd18078b099dcdac4d6420c5a5f
adhrianom <adhrianocanal@gmail.com>
2025-09-09 17:19:24 -0300

    troca de nome da variavel de salario_liquido para inss

--- calcular_inss.py
+++ calcular_inss.py
@@ -1,16 +1,16 @@
 import math
 
 salario_bruto = float(input())
-salario_liquido = 0
+inss = 0
 
 match salario_bruto:
     case n if salario_bruto <= 720.00:
-        salario_liquido = salario_bruto * 0.0765
+        inss = salario_bruto * 0.0765
     case n if salario_bruto > 720.00 and salario_bruto <= 1200.00:
-        salario_liquido = salario_bruto * 0.09
+        inss = salario_bruto * 0.09
     case n if salario_bruto > 1200.00 and salario_bruto <= 2400.00:
-        salario_liquido = salario_bruto * 0.11
+        inss = salario_bruto * 0.11
     case _ :
-        salario_liquido = (2400 * 0.11)
+        inss = (2400 * 0.11)
 
-print(salario_liquido)
+print(inss)
